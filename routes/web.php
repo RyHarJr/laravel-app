aeda6b5a2efdbf8e8e7ac35d01d8a08f16464292
[RyHarJR] <[ryhargans01@gmail.com]>
2026-05-12 21:51:39 +0700

    Litte Update

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\FakultasController;
 use App\Http\Controllers\PeriodeController;
 use App\Http\Controllers\BeritaController;
 use App\Http\Controllers\ProdiController;
 
-Route::get('/', fn() => redirect()->route('products.index'));
-Route::resource('products', ProductController::class);
+Route::get('/', fn() => redirect()->route('fakultas.index'));
 Route::resource('fakultas', FakultasController::class);
 Route::resource('periode', PeriodeController::class);
 Route::resource('berita', BeritaController::class);
