75d8ac8078586d6c1a37f63374122d4dea8273b1
[RyHarJR] <[ryhargans01@gmail.com]>
2026-06-09 12:21:42 +0700

    update auth

---
+++ resources/views/profile/edit.blade.php
@@ -0,0 +1,31 @@
+@extends('main')
+
+@section('title')
+    <h1>Form Edit Profile</h1>
+@endsection
+
+@section('content')
+    <x-app-layout>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                    <div class="max-w-xl">
+                        @include('profile.partials.update-profile-information-form')
+                    </div>
+                </div>
+
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                    <div class="max-w-xl">
+                        @include('profile.partials.update-password-form')
+                    </div>
+                </div>
+
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                    <div class="max-w-xl">
+                        @include('profile.partials.delete-user-form')
+                    </div>
+                </div>
+            </div>
+        </div>
+    </x-app-layout>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\FakultasController;
 use App\Http\Controllers\PeriodeController;
 use App\Http\Controllers\BeritaController;
@@ -9,9 +10,19 @@ use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\DashboardController;
 
 Route::get('/', fn() => redirect()->route('dashboard'));
-Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::resource('fakultas', FakultasController::class);
-Route::resource('periode', PeriodeController::class);
-Route::resource('berita', BeritaController::class);
-Route::resource('prodi', ProdiController::class);
-Route::resource('mahasiswa', MahasiswaController::class);
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('fakultas', FakultasController::class);
+    Route::resource('periode', PeriodeController::class);
+    Route::resource('berita', BeritaController::class);
+    Route::resource('prodi', ProdiController::class);
+    Route::resource('mahasiswa', MahasiswaController::class);
+});
+
+require __DIR__ . '/auth.php';
